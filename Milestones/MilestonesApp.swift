c0a7dae31fc4f2ef278ad4a955b32989b33b6e81
yeebruh21 <achen3741@gmail.com>
2022-06-21 20:20:20 -0400

    Initial Commit

---
+++ Milestones/MilestonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilestonesApp.swift
+//  Milestones
+//
+//  Created by Anthony Chen on 2022-06-21.
+//
+
+import SwiftUI
+
+@main
+struct MilestonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
